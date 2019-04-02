20102d0f78919b2e615a271618897f1217cf3b19
Max Pryakhin <m.pryakhin@gmail.com>
2019-04-02 23:43:16 +0300

    cs fix

--- public/index.php
+++ public/index.php
@@ -1,27 +1,35 @@
 <?php
 
-use App\Kernel;
+declare(strict_types = 1);
+
+namespace Frz;
+
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/config/bootstrap.php';
+require dirname(__DIR__) . '/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
+}
 
+if ($_SERVER['APP_DEBUG'] && class_exists(Debug::class)) {
     Debug::enable();
 }
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+    $headers = Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST;
+    Request::setTrustedProxies(explode(',', $trustedProxies), $headers);
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel  = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
+
 $response = $kernel->handle($request);
 $response->send();
+
 $kernel->terminate($request, $response);
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,7 +1,10 @@
 <?php
 
-namespace App;
+declare(strict_types = 1);
 
+namespace Frz;
+
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
@@ -9,40 +12,84 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
+/**
+ * Ядро приложения
+ */
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+    /**
+     * Допустимые расширения файлов конфигурации
+     *
+     * @const string
+     */
+    private const EXT = '.{php,xml,yaml,yml}';
 
+    /**
+     * Возвращает корневую директорию проекта
+     *
+     * @return string
+     */
+    public function getProjectDir()
+    {
+        return dirname(__DIR__);
+    }
+
+    /**
+     * Регистрирует модули приложения
+     *
+     * @return iterable
+     */
     public function registerBundles(): iterable
     {
-        $contents = require $this->getProjectDir().'/config/bundles.php';
+        $contents = require $this->getProjectDir() . '/config/bundles.php';
+        $env      = $this->getEnvironment();
+
         foreach ($contents as $class => $envs) {
-            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
+            if ($envs[$env] ?? $envs['all'] ?? false) {
                 yield new $class();
             }
         }
     }
 
+    /**
+     * Устанавливает параметры сервис-контейнера
+     *
+     * @param ContainerBuilder $container
+     * @param LoaderInterface  $loader
+     *
+     * @throws Exception
+     */
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
-        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
-        $container->setParameter('container.dumper.inline_class_loader', true);
-        $confDir = $this->getProjectDir().'/config';
+        $confDir = $this->getProjectDir() . '/config';
+        $env     = $this->getEnvironment();
 
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+        $container->addResource(new FileResource($confDir . '/bundles.php'));
+        $container->setParameter('container.dumper.inline_class_loader', true);
+
+        $loader->load($confDir . '/{packages}/*' . self::EXT, 'glob');
+        $loader->load($confDir . '/{packages}/' . $env . '/**/*' . self::EXT, 'glob');
+
+        $loader->load($confDir . '/{services}' . self::EXT, 'glob');
+        $loader->load($confDir . '/{services}_' . $env . self::EXT, 'glob');
     }
 
+    /**
+     * Устанавливает параметры маршрутизатора
+     *
+     * @param RouteCollectionBuilder $routes
+     *
+     * @throws Exception
+     */
     protected function configureRoutes(RouteCollectionBuilder $routes): void
     {
-        $confDir = $this->getProjectDir().'/config';
+        $confDir = $this->getProjectDir() . '/config';
+        $env     = $this->getEnvironment();
 
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/' . $env . '/**/*' . self::EXT, '/', 'glob');
+        $routes->import($confDir . '/{routes}/*' . self::EXT, '/', 'glob');
+        $routes->import($confDir . '/{routes}' . self::EXT, '/', 'glob');
     }
 }
